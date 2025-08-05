0dcf4036f3ed2be7861a3535e035836cb5be6a96
user0063 <toolsai897@gmail.com>
2025-08-05 19:51:58 +0530

    Update urls.py superuser

--- visual_dictionary/urls.py
+++ visual_dictionary/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from dictionary.views import create_admin_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dictionary.urls')),
-    path("create-admin/", create_admin_user)
 ]
